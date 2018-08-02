31883a3deee52c008bee6a3d816380bfecb62809
Max Guimaraes <ocgmax@gmail.com>
2018-08-01 22:54:19 -0300

    no message

--- SumupApi.js
+++
@@ -1,8 +0,0 @@
-import {
-    StackNavigator,
-} from 'react-navigation';
-import {HomeScreen} from './HomeScreen';
-const App = StackNavigator({
-    Home: { screen: HomeScreen },
-    Profile: { screen: ProfileScreen },
-});
